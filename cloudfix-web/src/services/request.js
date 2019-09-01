a7230e60e4d74f8abcc959aba6b6ceabc7bb9aa7
raidou12 <icarodaviduarte@gmail.com>
2019-09-01 17:53:13 -0300

    feat: axios request and response

--- cloudfix-web/src/services/request.js
+++ cloudfix-web/src/services/request.js
@@ -1,15 +1,39 @@
 import axios from 'axios';
+import cookie from 'react-cookies';
 
-const URL = process.env.URL || 'localhost:3000';
+const baseURL = process.env.URL || 'localhost:3000';
 
 const axiosRequest = axios.create({
-    URL,
+    baseURL,
     headers: {
         'Content-type': 'application/json',
         Accept: 'application/json'
     }
 })
 
-axiosRequest.interceptors
+axiosRequest.interceptors.request.use(
+    config => {
+        const token = cookie.load('jwt');
+        config.headers = {
+            ...config.headers,
+            Authorization: `Bearer ${token}`
+        };
+        return config;
+    },
+    error => Promise.reject(error)
+);
+
+axiosRequest.interceptors.response.use(
+    response => response,
+    error => {
+        if (!error.response) {
+            error.response = {
+                status: 444,
+                data: { message: "Ero ao mandar para o servidor." }
+            }
+        };
+        return Promise.reject(error);
+    }
+);
 
 export default axiosRequest;
