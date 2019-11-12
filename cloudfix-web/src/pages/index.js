4c5cf1a5975e70fac91dad51adb195012427222d
evillyn <evillyn.oliveira@estagiario.ufca.edu.br>
2019-11-12 17:04:18 -0300

    alterando Home do admin

--- cloudfix-web/src/pages/index.js
+++ cloudfix-web/src/pages/index.js
@@ -1,11 +1,12 @@
 import PageLayout from '../components/layout/page'
 import TicketForm from "../components/ticket/ticketForm/ticketForm";
+import TicketList from "../components/ticket/ticketList/ticketList";
 
 const Index = () => {
 
     return (
         <PageLayout>
-            <TicketForm />
+            <TicketList />
         </PageLayout>
     );
 }
