0b52dfdf1b3e6ab41b4e1b6e48da51d180731779
raidou12 <icarodaviduarte@gmail.com>
2019-12-05 02:52:11 -0300

    remove console log

--- cloudfix-api/src/controllers/ticketController.js
+++ cloudfix-api/src/controllers/ticketController.js
@@ -144,7 +144,6 @@ router.post('/addMessage', async (req, res) => {
             }
         });
     } catch (err) {
-        console.log(err)
         return res.status(400).send({ error: `Failed send message ${err}` });
     }
 });
