dc6c1e5d77c9564af4cf4d5ddf510d86db586a55
raidou12 <icarodaviduarte@gmail.com>
2019-12-08 16:17:36 -0300

    fix any bugs

--- cloudfix-web/src/components/myTicket/viewForm.js
+++ cloudfix-web/src/components/myTicket/viewForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Header, Container, Segment, Divider, Form, Grid, Button, Table, Comment } from 'semantic-ui-react';
+import { Header, Container, Segment, Divider, Form, Button, Table, Comment } from 'semantic-ui-react';
 import { Alert } from '../alert/alert';
 import TicketRequestor from '../../services/resources/ticket';
 import Router from 'next/router';
@@ -9,261 +9,260 @@ import lodash from 'lodash';
 import { loadToken } from '../../../src/lib/token';
 
 const MessageContainer = ({ message }) => (
-    <p style={{ fontSize: '1.4em' }}>
-        {message}
-    </p>
+  <p style={{ fontSize: '1.4em' }}>
+    {message}
+  </p>
 );
-  
-  const MyTicket = (props) => {
 
-    const [ticket, setTicket] = useState(
-      props.ticket
-    );
+const MyTicket = (props) => {
 
-    const [input, setInput] = useState({
-        message: ''
-    });
-  
-    const [load, setLoad] = useState(false);
-    
-    const [alert, setAlert] = useState(null);
+  const [ticket, setTicket] = useState(
+    props.ticket
+  );
 
-    const handleFildsChange = (e, { name, value }) => {
-      setInput({ ...input, [name]: value });
-    }
-    
+  const [input, setInput] = useState({
+    message: ''
+  });
+
+  const [load, setLoad] = useState(false);
+
+  const [alert, setAlert] = useState(null);
+
+  const handleFildsChange = (e, { name, value }) => {
+    setInput({ ...input, [name]: value });
+  }
+
   const sendMessage = async () => {
-    
-    console.log('send message');
     let check = {};
-    
+
     let validateInputs = Object.keys(check).every(element => {
       return check[element] === false;
     });
-    
+
     if (validateInputs) {
 
       setLoad(true);
-      
+
       let ticketObject = {
         message: input.message,
         ticketId: ticket._id,
         name: ticket.name
       }
 
-      let data = await TicketRequestor.addMessage(ticketObject);
+      await TicketRequestor.addMessage(ticketObject);
 
-      setTicket(data);
+      let data = await TicketRequestor.getTicket(ticketObject.ticketId);
 
-      data = await TicketRequestor.getTicket(ticketObject.ticketId);
-
-      console.log(data);
-      
-        if (!data.error) {
-          Router.push(`/myTicket?id=${data.id}`, '/ticket');
-        } else {
-          setAlert(<Alert buttonColor="red" iconTitle="warning" iconButton="checkmark" 
+      if (!data.error) {
+        setTicket(data.data);
+      } else {
+        setAlert(<Alert buttonColor="red" iconTitle="warning" iconButton="checkmark"
           message={data.error} open={true} title="Aviso" removeAlert={setAlert} />)
-        }
-        
-      setLoad(false);
       }
+
+      setLoad(false);
     }
+  }
 
-    const [checkInput, setCheckInput] = useState({ message: false });
+  const [checkInput, setCheckInput] = useState({ message: false });
 
-    const { name, system, title, message, status, updatedAt, images, chat } = ticket;
-    
-  const Row = (createdAt, message, name, index) => ( 
+  const { name, system, title, message, status, updatedAt, images, chat } = ticket;
+
+  const Row = (createdAt, message, name, index, userType) => (
     <Comment key={`${message}-${index}`} >
-      <Comment.Avatar content='{{ name.charAt(0) }}'/>
       <Comment.Content>
-        {/*{loadToken('token') ? <>
-        <Comment.Author as='a'>Administrador</Comment.Author></>
-         : <Comment.Author as='a'>{name}</Comment.Author>}
-        */}
-        <Comment.Author as='a'>Administrador</Comment.Author>
+        <Comment.Author as='a'>{userType === 'client' ? `Solicitante: ${name}` : `Administrador: ${name}` }</Comment.Author>
         <Comment.Metadata>
           <div>{moment(createdAt).fromNow()}</div>
         </Comment.Metadata>
         <Comment.Text>{message}</Comment.Text>
       </Comment.Content>
     </Comment>
-   );
-  
-    return (
-        <Container style={{ paddingBottom: "5em" }} textAlign='center'>
-            <Header as='h1' block>
-                Bem vindo ao seu ticket, <span style={{ fontWeight: 'bold' }}>{name}</span>!<br />
-                você escolheu o sistema <span style={{ textDecoration: 'underline' }}>{system}</span>
-            </Header>
+  );
 
-            <Container textAlign='justified'>
-              <Segment>
-                <Table basic='very' celled>
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.HeaderCell></Table.HeaderCell>
-                      <Table.HeaderCell></Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Header>
+  return (
+    <Container style={{ paddingBottom: "5em" }} textAlign='center'>
+      {alert}
+      <Header as='h1' block>
+        Bem vindo ao seu ticket, <span style={{ fontWeight: 'bold' }}>{name}</span>!<br />
+        você escolheu o sistema <span style={{ textDecoration: 'underline' }}>{system}</span>
+      </Header>
 
-                  <Table.Body>
-                     <Table.Row>
-                      <Table.Cell>
-                        <Header as='h4' image>
-                          <Header.Content>
-                            <Header as='h3'>
-                                Solicitante:
+      <Container textAlign='justified'>
+        <Segment>
+          <Table basic='very' celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell></Table.HeaderCell>
+                <Table.HeaderCell></Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4' image>
+                    <Header.Content>
+                      <Header as='h3'>
+                        Solicitante:
                             </Header>
-                          </Header.Content>
-                        </Header>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <MessageContainer message={name} />
-                      </Table.Cell>
-                    </Table.Row>
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  <MessageContainer message={name} />
+                </Table.Cell>
+              </Table.Row>
 
-                    <Table.Row>
-                      <Table.Cell>
-                        <Header as='h4' image>
-                          <Header.Content>
-                            <Header as='h3'>
-                                Sistema:
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4' image>
+                    <Header.Content>
+                      <Header as='h3'>
+                        Sistema:
                             </Header>
-                          </Header.Content>
-                        </Header>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <MessageContainer message={system} />
-                      </Table.Cell>
-                    </Table.Row>
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  <MessageContainer message={system} />
+                </Table.Cell>
+              </Table.Row>
 
-                    <Table.Row>
-                      <Table.Cell>
-                        <Header as='h4' image>
-                          <Header.Content>
-                            <Header as='h3'>
-                              Assunto do ticket:
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4' image>
+                    <Header.Content>
+                      <Header as='h3'>
+                        Assunto do ticket:
                             </Header>
-                          </Header.Content>
-                        </Header>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <MessageContainer message={title} />
-                      </Table.Cell>
-                    </Table.Row>
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  <MessageContainer message={title} />
+                </Table.Cell>
+              </Table.Row>
 
-                    {/*
-                    <Table.Row>
-                      <Table.Cell>
-                        <Header as='h4' image>
-                          <Header.Content>
-                          { images
-                          ? <Header as='h3'>
-                              Imagens do erro
+              {Array.isArray(images) && images.length !== 0
+                ? <Table.Row>
+                  <Table.Cell>
+                    <Header as='h4' image>
+                      <Header.Content>
+
+                        <Header as='h3'>
+                          Imagens do erro
                           </Header>
-                          : null}
-                          </Header.Content>
-                        </Header>
-                      </Table.Cell>
-                      <Table.Cell>
-                        {images ? images.map((img, i) => <a key={i} href={img} target="_blank"><MessageContainer message={`Imagem ${i+1}`} />  </a>) : null}
-                      </Table.Cell>
-                    </Table.Row>
-                    */}
 
-                    <Table.Row>
-                      <Table.Cell>
-                        <Header as='h4' image>
-                          <Header.Content>
-                            <Header as='h3'>
-                              Descrição:
+                      </Header.Content>
+                    </Header>
+                  </Table.Cell>
+                  <Table.Cell>
+                    {images ? images.map((img, i) => <a key={i} href={img} target="_blank"><MessageContainer message={`Imagem ${i + 1}`} />  </a>) : null}
+                  </Table.Cell>
+                </Table.Row>
+                : null}
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4' image>
+                    <Header.Content>
+                      <Header as='h3'>
+                        Descrição:
                             </Header>
-                          </Header.Content>
-                        </Header>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <MessageContainer message={message} />
-                      </Table.Cell>
-                    </Table.Row>
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  <MessageContainer message={message} />
+                </Table.Cell>
+              </Table.Row>
 
-                    <Table.Row>
-                      <Table.Cell>
-                        <Header as='h4' image>
-                          <Header.Content>
-                            <Header as='h3'>
-                                Status:
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4' image>
+                    <Header.Content>
+                      <Header as='h3'>
+                        Status:
                             </Header>
-                          </Header.Content>
-                        </Header>
-                      </Table.Cell>
-                      <Table.Cell positive={status === 'solved'} negative={status === 'closed'} >
-                        <MessageContainer message={status} />
-                      </Table.Cell>
-                    </Table.Row>
-                      
-                    <Table.Row>
-                      <Table.Cell>
-                        <Header as='h4' image>
-                          <Header.Content>
-                            <Header as='h3'>
-                                Última modificação:
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell positive={status === 'solved'} negative={status === 'closed'} >
+                  <MessageContainer message={status} />
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4' image>
+                    <Header.Content>
+                      <Header as='h3'>
+                        Última modificação:
                             </Header>
-                          </Header.Content>
-                        </Header>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <MessageContainer message={moment(updatedAt).fromNow()} />
-                      </Table.Cell>
-                    </Table.Row>
-                  </Table.Body>
-                </Table>
-                      
-                </Segment>
-                <Divider/>
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  <MessageContainer message={moment(updatedAt).fromNow()} />
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
 
-                          <Comment.Group>
-                            <Header as='h2' dividing>
-                            Acompanhe as atualizações do seu ticket
-                            </Header>
+        </Segment>
 
-                            <Comment.Group size='huge'>
-                              {lodash.map(chat, (e, i) => Row(e.createdAt, e.message, e.name, i))}
-                            </Comment.Group>
-                            
-                            <Divider/>
+        <Comment.Group>
+          {status === 'open'
+            ? <>
+              <Divider />
+              <Form reply>
+                <Form.Group>
+                  <Form.Field required width={16} >
+                    <Form.TextArea
+                      style={{ minHeight: 200, resize: 'none' }}
+                      icon="comment alternate outline"
+                      iconposition="left"
+                      placeholder="Digite uma mensagem..."
+                      error={checkInput.message ? { content: 'Digite alguma mensagem!' } : null}
+                      name='message'
+                      value={input.message}
+                      onChange={handleFildsChange}
+                    />
+                  </Form.Field>
+                </Form.Group>
+              </Form>
+              <Button
+                size='large'
+                positive
+                content="Enviar mensagem"
+                icon="right arrow"
+                labelPosition="right"
+                onClick={sendMessage}
+                disabled={load}
+                loading={load}
+              ></Button>
+            </>
+            : null}
 
-                              <Form reply>
-                                <Form.Group>
-                                  <Form.Field required width={16} >
-                                    <Form.TextArea
-                                      style={{ minHeight: 200 }}
-                                      icon="comment alternate outline"
-                                      iconposition="left"
-                                      placeholder="Digite uma mensagem..."
-                                      error={checkInput.message ? { content: 'Digite alguma mensagem!' } : null}
-                                      name='message'
-                                      value={input.message}
-                                      onChange={handleFildsChange}
-                                    />
-                                  </Form.Field>
-                                </Form.Group>
-                              </Form>
-                              <Button
-                                size='large'
-                                positive
-                                content="Enviar mensagem"
-                                icon="right arrow"
-                                labelPosition="right"
-                                onClick={sendMessage}
-                                disabled={load}
-                                loading={load}
-                              ></Button>
-                          </Comment.Group>                            
-              </Container>
-        </Container>
-    );
+          {Array.isArray(chat) && chat.length !== 0
+            ? <>
+              <Divider />
+              <Header as='h2' dividing>
+                Acompanhe as atualizações do seu ticket
+              </Header>
+
+              <Comment.Group size='huge'>
+                {(lodash.sortBy(chat, (o) => o.updatedAt).reverse().map((e, i) => Row(e.createdAt, e.message, e.name, i)))}
+              </Comment.Group>
+            </>
+            : null}
+
+
+        </Comment.Group>
+      </Container>
+    </Container>
+  );
 }
 
 export default MyTicket;
