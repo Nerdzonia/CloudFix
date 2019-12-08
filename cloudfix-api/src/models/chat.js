7ffd115ad340a39cc934dea94389309690c1b003
raidou12 <icarodaviduarte@gmail.com>
2019-12-08 16:16:33 -0300

    add userType

--- cloudfix-api/src/models/chat.js
+++ cloudfix-api/src/models/chat.js
@@ -9,6 +9,10 @@ const chatSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    userType: {
+        type: String,
+        required: true
+    },
     createdAt: {
         type: Date,
         default: Date.now,
